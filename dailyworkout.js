3ce0fd8260d422f44eed79eb2c3a03a26c9946c6
Bocahapel <pratamadrian.putra@gmail.com>
2025-12-11 17:33:16 +0700

    Create dailyworkout.js

---
+++ dailyworkout.js
@@ -0,0 +1,20 @@
+//Day 1, Condition
+
+const isAdult = function (age) {
+  const ageTest = Number(age);
+
+  if (ageTest < 0) {
+    console.log("Invalid Number");
+  }
+  if (ageTest < 18 && ageTest >= 0) {
+    console.log("Is not an adult");
+  }
+  if (ageTest >= 18) {
+    console.log("Is adult");
+  }
+
+  console.log(`The age: ${ageTest}`);
+  console.log(typeof ageTest);
+};
+
+isAdult("18");
